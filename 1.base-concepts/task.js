fd56d75202409c38ac6d00cedb3cadec183c1050
Ilya Gorshkov <gorshkov-new@yandex.ru>
2025-07-08 16:08:29 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+
+  const descrement = b ** 2 - 4 * a * c;
+
+  if (descrement < 0) {
+    return arr;
+  } else if (descrement === 0) {
+    return [-b / (2 * a)];
+  } else {
+    return [
+      (-b + Math.sqrt(descrement)) / (2 * a),
+      (-b - Math.sqrt(descrement)) / (2 * a),
+    ];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const interestRate = percent / 100 / 12;
+  const creditBody = amount - contribution;
+  const monthlyPayment =
+    creditBody *
+    (interestRate + interestRate / ((1 + interestRate) ** countMonths - 1));
+  const totalAmount = Number((monthlyPayment * countMonths).toFixed(2));
+
+  return totalAmount;
 }
