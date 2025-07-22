1007c8f529458d0cbf027faaf8ff1fe9807f1033
Ilya Gorshkov <gorshkov-new@yandex.ru>
2025-07-22 22:36:32 +0300

    Задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if (this.marks) {
+    this.marks.push(...marks);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  return (
+    this.marks?.reduce(
+      (accumulator, item) => accumulator + item / this.marks?.length,
+      0
+    ) || 0
+  );
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+};
